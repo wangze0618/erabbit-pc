4f8ddb356e7b4360ebde38342b45cfe6e7e40d0d
wangze0618 <2867273828@qq.com>
2022-03-04 23:41:31 +0800

    common.less

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap/dist/js/bootstrap.bundle.min'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import 'normalize.css'
+import '@/assets/styles/common.less'
 createApp(App).use(store).use(router).mount('#app')
