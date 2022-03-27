7e771fa539c31c02fe52df7ab9bb6b7ff4142c54
wangze0618 <2867273828@qq.com>
2022-03-27 23:52:11 +0800

    add

--- src/views/category/components/sub-sort.vue
+++ src/views/category/components/sub-sort.vue
@@ -1,14 +1,48 @@
 <template>
   <div class="sub-sort">
     <div class="sort">
-      <a href="javascript:;">默认排序</a>
-      <a href="javascript:;">最新商品</a>
-      <a href="javascript:;">最高人气</a>
-      <a href="javascript:;">评论最多</a>
-      <a href="javascript:;">
+      <a
+        :class="{ active: sortParams.sortField === null }"
+        @click="changeSort(null)"
+        href="javascript:;"
+        >默认排序</a
+      >
+      <a
+        :class="{ active: sortParams.sortField === 'publishTime' }"
+        @click="changeSort('publishTime')"
+        href="javascript:;"
+        >最新商品</a
+      >
+      <a
+        :class="{ active: sortParams.sortField === 'orderNum' }"
+        @click="changeSort('orderNum')"
+        href="javascript:;"
+        >最高人气</a
+      >
+      <a
+        :class="{ active: sortParams.sortField === 'evaluateNum' }"
+        @click="changeSort('evaluateNum')"
+        href="javascript:;"
+        >评论最多</a
+      >
+      <a href="javascript:;" @click="changeSort('price')">
         价格排序
-        <i class="arrow up" />
-        <i class="arrow down" />
+        <i
+          class="arrow up"
+          :class="{
+            active:
+              sortParams.sortField === 'price' &&
+              sortParams.sortMethod == 'asc',
+          }"
+        />
+        <i
+          class="arrow down"
+          :class="{
+            active:
+              sortParams.sortField === 'price' &&
+              sortParams.sortMethod == 'desc',
+          }"
+        />
       </a>
     </div>
     <div class="check">
@@ -18,7 +52,31 @@
   </div>
 </template>
 <script setup>
+import { reactive } from 'vue'
 import XtxCheckbox from '@/components/library/xtx-checkbox.vue'
+// 1. 根据后台需要的参数定义数据对象
+// 2. 根据数据对象，绑定组件（复选框，排序按钮）
+// 3. 在操作排序组件的时候，需要反馈给数据对象
+// sortField====>publishTime,orderNum,price,evaluateNum
+// sortMethod====>asc为正序 desc为倒序
+
+const sortParams = reactive({
+  inventory: false,
+  onlyDiscount: false,
+  sortField: null,
+  sortMethod: null,
+})
+// 改变排序
+const changeSort = (sortField) => {
+  if (sortField == 'price') {
+    if (sortParams.sortMethod == null) {
+      sortParams.sortMethod = 'desc'
+    } else {
+      sortParams.sortMethod = sortParams.sortMethod == 'asc' ? 'desc' : 'asc'
+    }
+  }
+  sortParams.sortField = sortField
+}
 </script>
 <style scoped lang="less">
 .sub-sort {
@@ -75,3 +133,4 @@ import XtxCheckbox from '@/components/library/xtx-checkbox.vue'
   }
 }
 </style>
+1
